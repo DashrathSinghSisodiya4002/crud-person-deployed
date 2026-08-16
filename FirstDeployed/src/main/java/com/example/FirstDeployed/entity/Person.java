1570fd62983ffb48b327c930b91f333ffca9002f
DASHRATHSINGHSISODIYA <dashrathsinghs574@gmail.com>
2026-08-16 15:54:55 +0530

    Update lombok entity changes

--- FirstDeployed/src/main/java/com/example/FirstDeployed/entity/Person.java
+++ FirstDeployed/src/main/java/com/example/FirstDeployed/entity/Person.java
@@ -8,8 +8,14 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
 	
 	@Id
@@ -20,61 +26,6 @@ public class Person {
 	private String email;
 	@Enumerated(EnumType.STRING)
 	private Gender gender;
-	
-	public Person() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Person(Long id, String name, LocalDate dob, String email, Gender gender) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.dob = dob;
-		this.email = email;
-		this.gender = gender;
-	}
-	public Person(String name, LocalDate dob, String email, Gender gender) {
-		super();
-		this.name = name;
-		this.dob = dob;
-		this.email = email;
-		this.gender = gender;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public LocalDate getDob() {
-		return dob;
-	}
-	public void setDob(LocalDate dob) {
-		this.dob = dob;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public Gender getGender() {
-		return gender;
-	}
-	public void setGender(Gender gender) {
-		this.gender = gender;
-	}
-	@Override
-	public String toString() {
-		return "Person [id=" + id + ", name=" + name + ", dob=" + dob + ", email=" + email + ", gender=" + gender + "]";
-	}
-	
-	
+		
 
 }
